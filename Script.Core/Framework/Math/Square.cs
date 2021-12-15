0054bb7fc40e4a545196f07bc50df5385c9e2cb3
Kristoffer Kjeldby <Kristoffer.Kjeldby@isobar.com>
2021-12-15 21:55:49 +0100

    Add mandelbrot script

---
+++ Script.Core/Framework/Math/Square.cs
@@ -0,0 +1,20 @@
+﻿namespace Script.Core.Framework.Math
+{
+    using Script.Core.Interpreter;
+    using Script.Core.Interpreter.Models;
+    using Script.Core.Models;
+
+    /// <summary>
+    /// Implementation of a square method
+    /// </summary>
+    [Method("sq", 1)]
+    public class Square : Method
+    {
+        /// <inheritdoc/>
+        public override string Execute(ExecutionContext context, params string[] parameters)
+        {
+            var a = Cast.ToDouble(parameters[0]);
+            return Cast.ToString(System.Math.Pow(a, 2));
+        }
+    }
+}
---
+++ Script.Core/Framework/Math/SquareRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace Script.Core.Framework.Math
+{
+    using Script.Core.Interpreter;
+    using Script.Core.Interpreter.Models;
+    using Script.Core.Models;
+
+    /// <summary>
+    /// Implementation of a square root method
+    /// </summary>
+    [Method("sqrt", 1)]
+    public class SquareRoot : Method
+    {
+        /// <inheritdoc/>
+        public override string Execute(ExecutionContext context, params string[] parameters)
+        {
+            var a = Cast.ToDouble(parameters[0]);
+            return Cast.ToString(System.Math.Pow(a, 0.5));
+        }
+    }
+}
